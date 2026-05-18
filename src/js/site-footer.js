1f911cd3e37817d75da951a35294eb01946bb1fe
Christian Webb <cawebb22@gmail.com>
2026-05-17 17:48:00 -0700

    feat: improve site footer component

--- src/js/site-footer.js
+++ src/js/site-footer.js
@@ -3,10 +3,18 @@ import { initExternalLinks } from "./external-links.js";
 
 class SiteFooter extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = `<footer>
-      <p><small>Christian Webb</small></p>
-      <p class="footer-links">${githubProfileLink()}${linkedinProfileLink()}</p>
-    </footer>`;
+    let footer = this.querySelector("footer");
+    if (!footer) {
+      footer = document.createElement("footer");
+      footer.innerHTML = `<p><small>Christian Webb</small></p><p class="footer-links"></p>`;
+      this.appendChild(footer);
+    }
+
+    const links = footer.querySelector(".footer-links");
+    if (links) {
+      links.innerHTML = `${githubProfileLink()}${linkedinProfileLink()}`;
+    }
+
     initExternalLinks(this);
   }
 }
